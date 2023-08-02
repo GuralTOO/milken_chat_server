86e2a524a54264444cf37b8bf5d88677f4529137
GuralTOO <guralben@gmail.com>
2023-08-02 13:00:15 -0400

    changed port to 8000

--- econ_club_app.py
+++ econ_club_app.py
@@ -39,4 +39,4 @@ def search(text_query):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=8001, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=8000, allow_unsafe_werkzeug=True)
