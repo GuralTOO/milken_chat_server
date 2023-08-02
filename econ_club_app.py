483fdb2038080952ef16b643a9e552d25266765a
GuralTOO <guralben@gmail.com>
2023-08-02 01:26:15 -0400

    adding ability to run server unsafely

--- econ_club_app.py
+++ econ_club_app.py
@@ -39,4 +39,4 @@ def search(text_query):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=8001)
+    socketio.run(app, host="0.0.0.0", port=8001, allow_unsafe_werkzeug=True)
